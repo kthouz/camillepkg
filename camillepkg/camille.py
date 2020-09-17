693a919d0c4506c90a7afa482d8dd93eec75c5af
Camille Girabawe <girabawe@adobe.com>
2020-09-17 11:49:20 -0500

    updates

--- camillepkg/camille.py
+++ camillepkg/camille.py
@@ -1,3 +1,1 @@
 from dependencies import hello
-if __name__ == '__main__':
-    hello(input())
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='camillepkg',
+      version='1.0',
+      description='pkg to test pip install gitrepo',
+      url='git@github.com:kthouz/camillepkg.git',
+      author='camille',
+      author_email='cgirabawe@gmail.com',
+      license='MIT',
+      packages=['camillepkg'],
+      zip_safe=False
+      )
